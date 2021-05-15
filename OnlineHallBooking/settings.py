62fb51f6fab3b22504b9567763c6009f91a8b3b6
email <yasminyash21@gmail.com>
2021-05-15 11:12:47 +0530

    update 15 may

--- OnlineHallBooking/settings.py
+++ OnlineHallBooking/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = "/Images/"
+MEDIA_URL = "/images/"
 MEDIA_ROOT = os.path.join(BASE_DIR,'HallBooking/static/images')
 LOGIN_URL = 'lg'
 LOGIN_REDIRECT_URL ='book'
